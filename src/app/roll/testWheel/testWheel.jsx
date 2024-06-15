b79528e8a3fd5473c90ae2daf6353ae4eaea77fa
MauricioStrang <strangmauricio@gmail.com>
2024-06-15 19:29:00 -0300

    Created test wheel page

---
+++ src/app/roll/testWheel/testWheel.jsx
@@ -0,0 +1,20 @@
+import styles from './testWheel.module.css'
+
+
+export const metadata = {
+    title: "Test Wheel - AniRoll",
+    description: "Test Wheel page",
+  };
+
+const TestWheelPage = () => {
+    return (
+        <div className={styles.container}>
+            <h1>
+                Test Wheel Page
+            </h1>
+        </div>
+    )
+};
+
+
+export default TestWheelPage;
