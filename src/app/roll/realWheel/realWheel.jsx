2c44d56afd4235a22e96c64c05a37f348ac4cbbe
MauricioStrang <strangmauricio@gmail.com>
2024-06-16 19:13:34 -0300

    added real wheel component

---
+++ src/app/roll/realWheel/realWheel.jsx
@@ -0,0 +1,20 @@
+import styles from './testWheel.module.css'
+
+
+export const metadata = {
+    title: "Anime Wheel - AniRoll",
+    description: "Anime Wheel page",
+  };
+
+const RealWheelPage = () => {
+    return (
+        <div className={styles.container}>
+            <h1>
+                Real Wheel Page
+            </h1>
+        </div>
+    )
+};
+
+
+export default RealWheelPage;
