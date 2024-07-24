a652426da785c75868fa4be6b793a66b23add2c5
MauricioStrang <strangmauricio@gmail.com>
2024-07-24 20:34:52 -0300

    testing api requests internet and local

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,14 +1,57 @@
 //Page for random testing of function and component behaviours
 
-import styles from "./testPage.module.css"
+
+import styles from './testPage.module.css'
+
+
 
 const testPage = async() => {
 
+
+    const getApiProducts = async()=>{
+        const res = await fetch('https://dummyjson.com/products')
+        return res.json()
+    }
+    
+    const apiData = await getApiProducts();
+
+    const getAppProducts = async()=>{
+        const res = await fetch('http://localhost:3000/api/')
+        return res.json()
+    }
+    
+    const appData = await getAppProducts();
+    
+
     return (
-        <div>
-            <h1>TESTS</h1>
-            
-        </div>    
+        <div className={styles.container}>
+            <h1>API PRODUCTS</h1>
+            <div>
+                <ul className={styles.list}>
+                    {apiData.products.map( ( item, index )=>(
+                            <li key={index}>
+                                <img src={item.thumbnail} alt={item.title} />
+                                <div>
+                                    {item.title} <span>${item.price}</span>
+                                </div>
+                            </li>
+                        ))}
+                </ul>
+            </div>
+            <h1>OWN APP PRODUCTS</h1>
+            <div>
+                <ul className={styles.list}>
+                    {appData.products.map( ( item, index )=>(
+                            <li key={index}>
+                                <img src={item.thumbnail} alt={item.title} />
+                                <div>
+                                    {item.title} <span>${item.price}</span>
+                                </div>
+                            </li>
+                        ))}
+                </ul>
+            </div>
+        </div>           
   );
 };
 
