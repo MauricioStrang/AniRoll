e783d5c44e37008b85dc0e7299c82cd8822966b2
MauricioStrang <strangmauricio@gmail.com>
2024-08-04 18:56:43 -0300

    commenting api user authorisation

--- src/components/authButton/AuthButton.js
+++ src/components/authButton/AuthButton.js
@@ -1,20 +1,24 @@
+//button component to authorise the user
+
 'use client'
-
-
 import { generateAuthUrl } from '@/lib/auth';
 import { generateCodeChallenge, generateCodeVerifier } from '@/lib/pkce';
 
+
+
 const AuthButton = () => {
-  const clientId = process.env.MAL_CLIENT_ID;
+  const clientId = process.env.MAL_CLIENT_ID; //passing the client id from the mal api
   const state = 'RequestID42'; // Optional
 
   const handleAuthClick = () => {
+    //generating the codeverfier and challenge
     const codeVerifier = generateCodeVerifier();
     const codeChallenge = generateCodeChallenge(codeVerifier);
 
     // Store codeVerifier in session storage to use it later in the callback handling
     sessionStorage.setItem('code_verifier', codeVerifier);
 
+    //generate function bellow passing the params for the full URL
     const authUrl = generateAuthUrl(clientId, codeChallenge, state);
     window.location.href = authUrl;
   };
